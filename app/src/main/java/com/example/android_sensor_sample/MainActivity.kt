6600994b1c9b2a95b2fd465cc4c3cf2c6200ff3c
Ananta Yudica <nta040689@gmail.com>
2019-12-21 00:43:20 +0700

    Added variable native of class Native Added update handler Added object runnable for call update function and delay 1 sec Added overriding function of android activity-lifecycle Added fuction update for call native update function

--- app/src/main/java/com/example/android_sensor_sample/MainActivity.kt
+++ app/src/main/java/com/example/android_sensor_sample/MainActivity.kt
@@ -2,12 +2,60 @@ package com.example.android_sensor_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
+    val native = Native()
+    lateinit var updateHandler : Handler
+    private val updateJob = object : Runnable {
+        override fun run() {
+            update()
+            updateHandler.postDelayed(this, 1000)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        native.onCreate()
         setContentView(R.layout.activity_main)
+        updateHandler = Handler(Looper.getMainLooper())
+    }
 
+    override fun onStart() {
+        super.onStart()
+        native.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        native.onResume()
+        updateHandler.post(updateJob)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        native.onPause()
+        updateHandler.removeCallbacks(updateJob)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        native.onStop()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        native.onRestart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        native.onDestroy()
+    }
+
+    fun update()
+    {
+        native.update()
     }
 
 }
