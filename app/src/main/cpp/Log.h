61c5eb28fae2d12219864507450ebcb42ec4aaba
Ananta Yudica <nta040689@gmail.com>
2019-12-21 20:51:56 +0700

    Fixed invalid preprocessing

--- app/src/main/cpp/Log.h
+++ app/src/main/cpp/Log.h
@@ -8,27 +8,27 @@
 #include <android/log.h>
 
 #define LOG(tag, format, ...)\
-    __android_log_print(ANDROID_LOG_DEFAULT, tag, format ##__VA_ARGS__)
+    __android_log_print(ANDROID_LOG_DEFAULT, tag, format, ##__VA_ARGS__)
 
 #define LOG_VERBOSE(tag, format, ...)\
-    __android_log_print(ANDROID_LOG_VERBOSE, tag, format ##__VA_ARGS__)
+    __android_log_print(ANDROID_LOG_VERBOSE, tag, format, ##__VA_ARGS__)
 
 #define LOG_DEBUG(tag, format, ...)\
-    __android_log_print(ANDROID_LOG_DEBUG, tag, format ##__VA_ARGS__)
+    __android_log_print(ANDROID_LOG_DEBUG, tag, format, ##__VA_ARGS__)
 
 #define LOG_INFO(tag, format, ...) \
-    __android_log_print(ANDROID_LOG_INFO, tag, format ##__VA_ARGS__)
+    __android_log_print(ANDROID_LOG_INFO, tag, format, ##__VA_ARGS__)
 
 #define LOG_WARN(tag, format, ...)\
-    __android_log_print(ANDROID_LOG_WARN, tag, format ##__VA_ARGS__)
+    __android_log_print(ANDROID_LOG_WARN, tag, format, ##__VA_ARGS__)
 
 #define LOG_ERROR(tag, format, ...)\
-    __android_log_print(ANDROID_LOG_ERROR, tag, format ##__VA_ARGS__)
+    __android_log_print(ANDROID_LOG_ERROR, tag, format, ##__VA_ARGS__)
 
 #define LOG_FATAL(tag, format, ...)\
-    __android_log_print(ANDROID_LOG_FATAL, tag, format ## __VA_ARGS__)
+    __android_log_print(ANDROID_LOG_FATAL, tag, format, ## __VA_ARGS__)
 
 #define LOG_SILENT(tag, format, ...)\
-    __android_log_print(ANDROID_LOG_SILENT, tag, format ##__VA_ARGS__)
+    __android_log_print(ANDROID_LOG_SILENT, tag, format, ##__VA_ARGS__)
 
 #endif //ANDROID_SENSOR_SAMPLE_LOG_H
