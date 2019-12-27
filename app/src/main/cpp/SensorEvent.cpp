62f29269093aec8b95953e9863e8c08e92af0fcc
Ananta Yudica <nta040689@gmail.com>
2019-12-28 05:10:20 +0700

    Added class SensorEvent for handle thread and Android Sensor Event Queue

---
+++ app/src/main/cpp/SensorEvent.cpp
@@ -0,0 +1,391 @@
+//
+// Created by Ananta Yudica on 27/12/2019.
+//
+
+#include "SensorEvent.h"
+#include "Log.h"
+
+#include <chrono>
+
+constexpr const int SensorEvent::OK;
+constexpr const int SensorEvent::ERROR_NO_SENSOR;
+constexpr const int SensorEvent::ERROR_NO_SENSORMANAGER;
+constexpr const int SensorEvent::ERROR_PREPARE_LOOPER;
+constexpr const int SensorEvent::ERROR_CREATE_EVENTQUEUE;
+constexpr const int SensorEvent::ERROR_INIT_THREAD;
+constexpr const int SensorEvent::ERROR_ENABLE_SENSOR;
+constexpr const int SensorEvent::ERROR_DISABLE_SENSOR;
+constexpr const int SensorEvent::ERROR_SET_EVENT_RATE;
+constexpr const int SensorEvent::ERROR_HAS_EVENTS;
+constexpr const int SensorEvent::ERROR_GET_EVENTS;
+
+SensorEvent::SensorEvent(ASensorManager * pSensorManager, Sensor * pSensor) :
+    m_delay(-1),
+    m_error(OK),
+    m_flag(0),
+    m_sensormanager(pSensorManager),
+    m_sensor(pSensor),
+    m_eventQueue(nullptr),
+    m_looper(nullptr),
+    m_thread(nullptr),
+    m_data(nullptr)
+{
+    LOG_DEBUG("SensorEvent", "Constructor(...)");
+    Init();
+}
+
+SensorEvent::SensorEvent(ASensorManager * pSensorManager, Sensor * pSensor,
+    const int& delay) :
+        m_delay(-1),
+        m_error(OK),
+        m_flag(0),
+        m_sensormanager(pSensorManager),
+        m_sensor(pSensor),
+        m_eventQueue(nullptr),
+        m_looper(nullptr),
+        m_thread(nullptr),
+        m_data(nullptr)
+{
+    LOG_DEBUG("SensorEvent", "Constructor(...)");
+    Init(delay);
+}
+
+SensorEvent::~SensorEvent()
+{
+    LOG_DEBUG("SensorEvent", "Destructor(...)");
+    Stop();
+    m_error = OK;
+    m_delay = -1;
+    m_flag.store(0, std::memory_order_relaxed);
+    m_sensormanager = nullptr;
+    m_sensor = nullptr;
+}
+
+int SensorEvent::Init(const int& delay)
+{
+    LOG_DEBUG("SensorEvent", "Init(...)");
+    if (!m_sensor) return __SetError<int>(this, m_error, ERROR_NO_SENSOR, -1);
+    else m_delay = delay >= m_sensor->MinDelay() ? delay : m_sensor->MinDelay();
+    if (!m_sensormanager) return __SetError<int>(this, m_error, ERROR_NO_SENSORMANAGER, -2);
+    return 0;
+}
+
+int SensorEvent::InitThread()
+{
+    LOG_DEBUG("SensorEvent", "InitThread(...)");
+    m_thread = new std::thread(&SensorEvent::Run, this);
+    LOG_DEBUG("SensorEvent", "thread : %p", (void*)m_thread);
+    if (!m_thread)
+        return __SetError<int>(this, m_error, ERROR_INIT_THREAD, -1);
+    return 1;
+}
+
+int SensorEvent::InitLooper()
+{
+    LOG_DEBUG("SensorEvent", "InitLooper(...)");
+    m_looper = ALooper_prepare(ALOOPER_PREPARE_ALLOW_NON_CALLBACKS);
+    LOG_DEBUG("SensorEvent", "looper : %p", (void*)m_looper);
+    if (!m_looper)
+        return __SetError<int>(this, m_error, ERROR_PREPARE_LOOPER, -1);
+    ALooper_acquire(m_looper);
+    return 1;
+}
+
+int SensorEvent::InitEventQueue()
+{
+    LOG_DEBUG("SensorEvent", "InitEventQueue(...)");
+    m_eventQueue = ASensorManager_createEventQueue(m_sensormanager, m_looper,
+            0, nullptr, m_data);
+    LOG_DEBUG("SensorEvent", "eventQueue : %p", (void*)m_eventQueue);
+    if (!m_eventQueue)
+        return __SetError<int>(this, m_error, ERROR_CREATE_EVENTQUEUE, -1);
+    return 1;
+}
+
+int SensorEvent::InitEnableSensor()
+{
+    LOG_DEBUG("SensorEvent", "InitEnableSensor(...)");
+    if (ASensorEventQueue_enableSensor(m_eventQueue, m_sensor->Pointer()) != 0)
+        return __SetError<int>(this, m_error, ERROR_ENABLE_SENSOR, -1);
+    return 0;
+}
+
+int SensorEvent::InitDisableSensor()
+{
+    LOG_DEBUG("SensorEvent", "InitDisableSensor(...)");
+    if (ASensorEventQueue_disableSensor(m_eventQueue, m_sensor->Pointer()) != 0)
+        return __SetError<int>(this, m_error, ERROR_DISABLE_SENSOR, -1);
+    return 0;
+}
+
+int SensorEvent::InitEventRate()
+{
+    LOG_DEBUG("SensorEvent", "InitEventRate(...)");
+    if  (ASensorEventQueue_setEventRate(m_eventQueue, m_sensor->Pointer(), m_delay) != 0)
+        return __SetError<int>(this, m_error, ERROR_SET_EVENT_RATE, -1);
+    LOG_DEBUG("SensorEvent", "set delay %d microseconds", m_delay);
+    return 0;
+}
+
+void SensorEvent::InitEnable()
+{
+    LOG_DEBUG("SensorEvent", "InitEnable(...)");
+    if (!InitEventQueue()) return;
+    if (!InitEnableSensor() || !InitEventRate())
+    {
+        ReleaseEventQueue();
+        return;
+    }
+    SetEnable();
+}
+
+void SensorEvent::InitDisable()
+{
+    LOG_DEBUG("SensorEvent", "InitDisable(...)");
+    InitDisableSensor();
+    ReleaseEventQueue();
+    SetDisable();
+}
+
+void SensorEvent::InitPause()
+{
+    LOG_DEBUG("SensorEvent", "InitPause(...)");
+    if (IsEnable()) InitDisableSensor();
+    SetPause();
+}
+
+void SensorEvent::InitResume()
+{
+    LOG_DEBUG("SensorEvent", "InitResume(...)");
+    if (IsEnable()) InitEnableSensor();
+    SetResume();
+}
+
+void SensorEvent::ReleaseThread()
+{
+    LOG_DEBUG("SensorEvent", "ReleaseThread(...)");
+    if (m_thread)
+    {
+        m_thread->join();
+        delete m_thread;
+        m_thread = nullptr;
+    }
+}
+
+void SensorEvent::ReleaseLooper()
+{
+    LOG_DEBUG("SensorEvent", "ReleaseLooper(...)");
+    if (!m_looper) return;
+    ALooper_release(m_looper);
+    m_looper = nullptr;
+}
+
+void SensorEvent::ReleaseEventQueue()
+{
+    LOG_DEBUG("SensorEvent", "ReleaseEventQueue(...)");
+    if (!m_eventQueue) return;
+    ASensorManager_destroyEventQueue(m_sensormanager, m_eventQueue);
+    m_eventQueue = nullptr;
+    m_data = nullptr;
+}
+
+void SensorEvent::Run()
+{
+    LOG_DEBUG("SensorEvent", "Run(...)");
+    if (!InitLooper()) return;
+    if (!IsEnable() && IsPrepareEnable())
+        InitEnable();
+    ASensorEvent * events = new ASensorEvent[10];
+    SetStart();
+    while(!IsError() && !IsRun())
+    {
+        const auto is_prepare_pause = IsPreparePause();
+        const auto is_pause = IsPause();
+        if (is_prepare_pause != is_pause)
+        {
+            if (is_prepare_pause) InitPause();
+            else InitResume();
+        }
+        if (is_pause)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(700));
+            continue;
+        }
+        if (!IsEnable() || !IsPrepareEnable()) continue;
+        auto has_events = ASensorEventQueue_hasEvents(m_eventQueue);
+        if(has_events < 0)
+        {
+            __SetError<void>(this, m_error, ERROR_HAS_EVENTS);
+            continue;
+        }
+        else if (has_events == 0) continue;
+        const auto size = ASensorEventQueue_getEvents(m_eventQueue, events, 10);
+        if (size < 0)
+        {
+            __SetError<void>(this, m_error, ERROR_GET_EVENTS);
+            continue;
+        }
+
+    }
+    SetStop();
+    delete[] events;
+    if (IsEnable()) InitDisable();
+    ReleaseLooper();
+}
+
+int SensorEvent::Delay() const
+{
+    return m_delay;
+}
+
+void SensorEvent::Delay(const int & val)
+{
+    LOG_DEBUG("SensorEvent", "Delay(...)");
+    if (!m_sensor) return;
+    if (val >= m_sensor->MinDelay())
+        m_delay = val;
+    if (IsRun() && IsEnable()) InitEventRate();
+    LOG_DEBUG("SensorEvent", "delya : %d", m_delay);
+}
+
+int SensorEvent::LastErrorCode() const
+{
+    return m_error;
+}
+
+void SensorEvent::Enable()
+{
+    LOG_DEBUG("SensorEvent", "Enable(...)");
+    if (IsPrepareEnable()) return;
+    SetPrepareEnable();
+    if (IsRun()) InitEnable();
+}
+
+void SensorEvent::Disable()
+{
+    LOG_DEBUG("SensorEvent", "Disable(...)");
+    if (!IsPrepareEnable()) return;
+    SetPrepareDisable();
+    if (IsRun()) InitDisable();
+}
+
+void SensorEvent::Pause()
+{
+    LOG_DEBUG("SensorEvent", "Pause(...)");
+    if (IsPreparePause()) return;
+    SetPreparePause();
+}
+
+void SensorEvent::Resume()
+{
+    LOG_DEBUG("SensorEvent", "Resume(...)");
+    if (!IsPreparePause()) return;
+    SetPrepareResume();
+}
+
+void SensorEvent::Start()
+{
+    LOG_DEBUG("SensorEvent", "Start(...)");
+    if (IsRun() || m_thread) return;
+    if (!InitThread()) SetStop();
+}
+
+void SensorEvent::Stop()
+{
+    LOG_DEBUG("SensorEvent", "Stop(...)");
+    if (!IsRun()) return;
+    SetStop();
+    ReleaseThread();
+}
+
+void SensorEvent::SetError()
+{
+    m_flag.fetch_or(FLAG_ERROR_SENSOR, std::memory_order_release);
+}
+
+void SensorEvent::SetPrepareEnable()
+{
+    m_flag.fetch_or(FLAG_PREPARE_ENABLE_SENSOR, std::memory_order_release);
+}
+
+void SensorEvent::SetPrepareDisable()
+{
+    m_flag.fetch_and(~FLAG_PREPARE_ENABLE_SENSOR, std::memory_order_release);
+}
+
+void SensorEvent::SetEnable()
+{
+    m_flag.fetch_or(FLAG_ENABLE_SENSOR, std::memory_order_release);
+}
+
+void SensorEvent::SetDisable()
+{
+    m_flag.fetch_and(~FLAG_ENABLE_SENSOR, std::memory_order_release);
+}
+
+void SensorEvent::SetPreparePause()
+{
+    m_flag.fetch_or(FLAG_PREPARE_PAUSE_SENSOR, std::memory_order_release);
+}
+
+void SensorEvent::SetPrepareResume()
+{
+    m_flag.fetch_and(~FLAG_PREPARE_PAUSE_SENSOR, std::memory_order_release);
+}
+
+void SensorEvent::SetPause()
+{
+    m_flag.fetch_or(FLAG_PAUSE_SENSOR, std::memory_order_release);
+}
+
+void SensorEvent::SetResume()
+{
+    m_flag.fetch_and(~FLAG_PAUSE_SENSOR, std::memory_order_release);
+}
+
+void SensorEvent::SetStart()
+{
+    m_flag.fetch_or(FLAG_RUN_SENSOR, std::memory_order_release);
+}
+
+void SensorEvent::SetStop()
+{
+    m_flag.fetch_and(~FLAG_RUN_SENSOR, std::memory_order_release);
+}
+
+bool SensorEvent::IsPrepareEnable() const
+{
+    return m_flag.load(std::memory_order_consume) &
+           FLAG_PREPARE_ENABLE_SENSOR;
+}
+
+bool SensorEvent::IsPreparePause() const
+{
+    return m_flag.load(std::memory_order_consume) &
+           FLAG_PREPARE_PAUSE_SENSOR;
+}
+
+bool SensorEvent::IsError() const
+{
+    return m_flag.load(std::memory_order_consume) <
+        std::int8_t(0);
+}
+
+bool SensorEvent::IsEnable() const
+{
+    return m_flag.load(std::memory_order_consume) &
+        FLAG_ENABLE_SENSOR;
+}
+
+bool SensorEvent::IsPause() const
+{
+    return m_flag.load(std::memory_order_consume) &
+        FLAG_PAUSE_SENSOR;
+}
+
+bool SensorEvent::IsRun() const
+{
+    return m_flag.load(std::memory_order_consume) &
+        FLAG_RUN_SENSOR;
+}
+
---
+++ app/src/main/cpp/SensorEvent.h
@@ -0,0 +1,135 @@
+//
+// Created by Ananta Yudica on 27/12/2019.
+//
+
+#ifndef ANDROID_SENSOR_SAMPLE_SENSOREVENT_H
+#define ANDROID_SENSOR_SAMPLE_SENSOREVENT_H
+
+#include <android/sensor.h>
+#include <android/looper.h>
+#include <thread>
+#include <atomic>
+#include <cstdint>
+#include <type_traits>
+
+#include "Sensor.h"
+
+class SensorEvent
+{
+private:
+    static constexpr const std::int8_t FLAG_PREPARE_ENABLE_SENSOR = 0x01;
+    static constexpr const std::int8_t FLAG_ENABLE_SENSOR = 0x02;
+    static constexpr const std::int8_t FLAG_PREPARE_PAUSE_SENSOR = 0x04;
+    static constexpr const std::int8_t FLAG_PAUSE_SENSOR = 0x08;
+    static constexpr const std::int8_t FLAG_RUN_SENSOR = 0x10;
+    static constexpr const std::int8_t FLAG_ERROR_SENSOR = 0x80;
+public:
+    static constexpr const int OK = 0x00;
+    static constexpr const int ERROR_NO_SENSOR = 0x01;
+    static constexpr const int ERROR_NO_SENSORMANAGER = 0x02;
+    static constexpr const int ERROR_PREPARE_LOOPER = 0x03;
+    static constexpr const int ERROR_CREATE_EVENTQUEUE = 0x04;
+    static constexpr const int ERROR_INIT_THREAD = 0x05;
+    static constexpr const int ERROR_ENABLE_SENSOR = 0x06;
+    static constexpr const int ERROR_DISABLE_SENSOR = 0x07;
+    static constexpr const int ERROR_SET_EVENT_RATE = 0x08;
+    static constexpr const int ERROR_HAS_EVENTS = 0x09;
+    static constexpr const int ERROR_GET_EVENTS = 0x0A;
+private:
+    template<typename TRet>
+    static typename std::enable_if<std::is_same<TRet, void>::value, TRet>::type
+    __SetError(SensorEvent * pThis, int & err, const int & code);
+    template<typename TRet>
+    static typename std::enable_if<!std::is_same<TRet, void>::value, TRet>::type
+    __SetError(SensorEvent * pThis, int & err, const int & code, const TRet & ret);
+private:
+    int m_delay;
+    int m_error;
+    std::atomic<std::int8_t> m_flag;
+    ASensorManager * m_sensormanager;
+    Sensor * m_sensor;
+    ASensorEventQueue * m_eventQueue;
+    ALooper * m_looper;
+    std::thread * m_thread;
+    void * m_data;
+public:
+    SensorEvent(ASensorManager * pSensorManager, Sensor * pSensor);
+    SensorEvent(ASensorManager * pSensorManager, Sensor * pSensor,
+            const int& delay);
+    ~SensorEvent();
+public:
+    SensorEvent(const SensorEvent &) = delete;
+    SensorEvent(SensorEvent &&) = delete;
+private:
+    int Init(const int& delay = -1);
+    int InitThread();
+    int InitLooper();
+    int InitEventQueue();
+    int InitEnableSensor();
+    int InitDisableSensor();
+    int InitEventRate();
+    void InitEnable();
+    void InitDisable();
+    void InitPause();
+    void InitResume();
+private:
+    void ReleaseThread();
+    void ReleaseLooper();
+    void ReleaseEventQueue();
+private:
+    void Run();
+public:
+    int Delay() const;
+    void Delay(const int & val);
+public:
+    int LastErrorCode() const;
+public:
+    void Enable();
+    void Disable();
+    void Pause();
+    void Resume();
+    void Start();
+    void Stop();
+private:
+    void SetError();
+    void SetPrepareEnable();
+    void SetPrepareDisable();
+    void SetEnable();
+    void SetDisable();
+    void SetPreparePause();
+    void SetPrepareResume();
+    void SetPause();
+    void SetResume();
+    void SetStart();
+    void SetStop();
+private:
+    bool IsPrepareEnable() const;
+    bool IsPreparePause() const;
+public:
+    bool IsError() const;
+    bool IsEnable() const;
+    bool IsPause() const;
+    bool IsRun() const;
+public:
+    SensorEvent & operator=(const SensorEvent &) = delete;
+    SensorEvent & operator=(SensorEvent &&) = delete;
+};
+
+template<typename TRet>
+typename std::enable_if<std::is_same<TRet, void>::value, TRet>::type
+SensorEvent::__SetError(SensorEvent * pThis, int & err, const int & code)
+{
+    err = code;
+    pThis->SetError();
+}
+
+template<typename TRet>
+typename std::enable_if<!std::is_same<TRet, void>::value, TRet>::type
+SensorEvent::__SetError(SensorEvent * pThis, int & err, const int & code, const TRet & ret)
+{
+    err = code;
+    pThis->SetError();
+    return ret;
+}
+
+#endif //ANDROID_SENSOR_SAMPLE_SENSOREVENT_H
