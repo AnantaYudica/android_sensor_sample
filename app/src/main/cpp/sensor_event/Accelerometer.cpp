e5e57d2bc6f21e4626dce1f42262710063ab6417
Ananta Yudica <nta040689@gmail.com>
2019-12-29 05:30:18 +0700

    Added class Accelerometer for handle sensor event of type Accelerometer

---
+++ app/src/main/cpp/sensor_event/Accelerometer.cpp
@@ -0,0 +1,59 @@
+//
+// Created by Ananta Yudica on 29/12/2019.
+//
+
+#include "Accelerometer.h"
+
+#include <utility>
+
+namespace sensor_event
+{
+
+Accelerometer::Accelerometer() :
+    Default{}
+{}
+
+Accelerometer::Accelerometer(std::shared_ptr<ASensorEvent> sensorEvents,
+    const size_t & size) :
+        Default(sensorEvents, size)
+{}
+
+Accelerometer::~Accelerometer()
+{}
+
+Accelerometer::Accelerometer(const Accelerometer & cpy) :
+    Default(cpy)
+{}
+
+Accelerometer::Accelerometer(Accelerometer && mov) :
+    Default(std::move(mov))
+{}
+
+Accelerometer & Accelerometer::operator=(const Accelerometer & cpy)
+{
+    Default::operator=(cpy);
+    return *this;
+}
+
+Accelerometer & Accelerometer::operator=(Accelerometer && mov)
+{
+    Default::operator=(std::move(mov));
+    return *this;
+}
+
+const float & Accelerometer::X(const size_t & i) const
+{
+    return m_sensorEvents.get()[i].acceleration.x;
+}
+
+const float & Accelerometer::Y(const size_t & i) const
+{
+    return m_sensorEvents.get()[i].acceleration.y;
+}
+
+const float & Accelerometer::Z(const size_t & i) const
+{
+    return m_sensorEvents.get()[i].acceleration.z;
+}
+
+} //!sensor_event
---
+++ app/src/main/cpp/sensor_event/Accelerometer.h
@@ -0,0 +1,37 @@
+//
+// Created by Ananta Yudica on 29/12/2019.
+//
+
+#ifndef ANDROID_SENSOR_SAMPLE_SENSOREVENT_ACCELEROMETER_H
+#define ANDROID_SENSOR_SAMPLE_SENSOREVENT_ACCELEROMETER_H
+
+#include "Default.h"
+
+#include <memory>
+#include <android/sensor.h>
+
+namespace sensor_event
+{
+
+class Accelerometer : public Default
+{
+public:
+    Accelerometer();
+    Accelerometer(std::shared_ptr<ASensorEvent> sensorEvents, const size_t & size);
+public:
+    ~Accelerometer();
+public:
+    Accelerometer(const Accelerometer & cpy);
+    Accelerometer(Accelerometer && mov);
+public:
+    Accelerometer & operator=(const Accelerometer & cpy);
+    Accelerometer & operator=(Accelerometer && mov);
+public:
+    const float & X(const size_t & i = 0) const;
+    const float & Y(const size_t & i = 0) const;
+    const float & Z(const size_t & i = 0) const;
+};
+
+} //!sensor_event
+
+#endif //ANDROID_SENSOR_SAMPLE_SENSOREVENT_ACCELEROMETER_H
