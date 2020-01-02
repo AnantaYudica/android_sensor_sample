d86d2ecaf9f49105c638d1c8139a42ea3a2a77d6
Ananta Yudica <nta040689@gmail.com>
2020-01-03 02:34:49 +0700

    Added Singleton class SensorManager for access SensorManager from native

---
+++ app/src/main/java/com/example/android_sensor_sample/SensorManager.kt
@@ -0,0 +1,40 @@
+package com.example.android_sensor_sample
+
+
+class SensorManager
+{
+    companion object
+    {
+        @Volatile private var instance : SensorManager = SensorManager()
+        @JvmStatic external fun hasInstance() : Boolean
+        fun getInstance() : SensorManager
+        {
+            return instance
+        }
+    }
+    init
+    {
+        System.loadLibrary("Bridge")
+    }
+    external fun size() : Int
+    external fun findOne(sensor_type: Int) : Int
+    external fun find(sensor_type : Int) : IntArray
+    external fun type(id : Int) : Int
+    external fun minDelay(id : Int) : Int
+    external fun resolution(id : Int) : Float
+    external fun name(id : Int) : String
+    external fun vendor(id : Int) : String
+    external fun lastErrorCode(id : Int) : Int
+    external fun delay(id : Int, value : Int) : Void
+    external fun delay(id : Int) : Int
+    external fun enable(id : Int) : Void
+    external fun disable(id : Int) : Void
+    external fun pause(id : Int) : Void
+    external fun resume(id : Int) : Void
+    external fun start(id : Int) : Void
+    external fun stop(id : Int) : Void
+    external fun isError(id : Int) : Boolean
+    external fun isEnable(id : Int) : Boolean
+    external fun isPause(id : Int) : Boolean
+    external fun isRun(id : Int) : Boolean
+}
