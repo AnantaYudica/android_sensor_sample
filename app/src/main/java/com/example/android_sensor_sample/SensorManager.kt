168b09acbb144d3f10f1b3f9b2556d0e0a76e07a
Ananta Yudica <nta040689@gmail.com>
2020-01-08 12:00:23 +0700

    Added declaration external member functions

--- app/src/main/java/com/example/android_sensor_sample/SensorManager.kt
+++ app/src/main/java/com/example/android_sensor_sample/SensorManager.kt
@@ -37,4 +37,6 @@ class SensorManager
     external fun isEnable(id : Int) : Boolean
     external fun isPause(id : Int) : Boolean
     external fun isRun(id : Int) : Boolean
+    external fun link(id : Int, obj : Any, name : String)
+    external fun unlink(id : Int)
 }
